6182f99e185d2628c7e55552927e000b1a8b157d
Camillus Anko <ankocamillus@gmail.com>
2025-07-24 09:53:22 +0000

    Day 24: From CSV to PDF

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from fpdf import FPDF
-
+import pandas as pd
 #We create a pdf object
 
 pdf = FPDF(orientation="L", unit="mm", format="A4")
 
+#Create a dataframe object
+
+df = pd.read_csv("topics.csv")
+
+#Now we iterate
+
+for index, row in df.iterrows():
+    pdf.add_page() #Used to add pages
+
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_text_color(100, 100, 100)
+
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
+    ln=1, border=1)
+
+    pdf.line(10, 21, 200, 21)
+    
 #Now we have a pdf instance
 
-pdf.add_page() #Used to add pages
-
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello There!", align="L",
- ln=1, border=1)
-
-pdf.set_font(family="Times", style="B", size=10)
-pdf.cell(w=0, h=12, txt="Hi There!", align="L",
- ln=1, border=1)
 pdf.output("output.pdf")
+
+
+
+
+
+# pdf.set_font(family="Times", style="B", size=12)
+# pdf.cell(w=0, h=12, txt="Hello There!", align="L",
+#  ln=1, border=1)
+
+# pdf.set_font(family="Times", style="B", size=10)
+# pdf.cell(w=0, h=12, txt="Hi There!", align="L",
+#  ln=1, border=1)
